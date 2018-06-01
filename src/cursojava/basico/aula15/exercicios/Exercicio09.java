468e386195f86b4ab000316bfd9eb885a6b7f3f5
Yuri Geovani <yuri_geovani@hotmail.com>
2018-05-31 21:31:47 -0300

    Exercício 9

---
+++ src/cursojava/basico/aula15/exercicios/Exercicio09.java
@@ -0,0 +1,43 @@
+package cursojava.basico.aula15.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio09 {
+
+	public static void main(String[] args) {
+
+//		9. Faça um Programa que leia três números e mostre-os em ordem
+//		decrescente.
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Informe o número 1: ");
+		double num1 = scan.nextDouble();
+		System.out.print("Informe o número 2: ");
+		double num2 = scan.nextDouble();
+		System.out.print("Informe o número 3: ");
+		double num3 = scan.nextDouble();
+		
+		if (num1<num2 && num1<num3) {
+			if (num2<num3) {
+				System.out.println("Números decrescente: " + num3 + ", " + num2 + ", " + num1);
+			} else {
+				System.out.println("Números decrescente: " + num2 + ", " + num3 + ", " + num1);
+			} 
+		} else if (num2<num3 && num2<num1) {
+			if (num3<num1) {
+				System.out.println("Números decrescente: " + num1 + ", " + num3 + ", " + num2);
+			} else {
+				System.out.println("Números decrescente: " + num3 + ", " + num1 + ", " + num2);
+			}
+		} else {
+			if (num1<num2) {
+				System.out.println("Números decrescente: " + num1 + ", " + num2 + ", " + num3);
+			} else {
+				System.out.println("Números decrescente: " + num2 + ", " + num1 + ", " + num3);
+			}
+		}
+
+	}
+
+}
