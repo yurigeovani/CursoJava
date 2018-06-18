e7d29618e9678c9ca96f687a40ae181a1e8b448e
Yuri Geovani <yuri_geovani@hotmail.com>
2018-06-17 22:58:45 -0300

    Conclusão exercício 15 e iníncio do exercício 16 da aula 17.

--- src/cursojava/basico/aula17/exercicios/Exercicio16.java
+++ src/cursojava/basico/aula17/exercicios/Exercicio16.java
@@ -2,12 +2,13 @@ package cursojava.basico.aula17.exercicios;
 
 import java.util.Scanner;
 
-public class Exercicio15 {
+public class Exercicio16 {
 
 	public static void main(String[] args) {
 
-//		15. A série de Fibonacci é formada pela seqüência 0,1,1,2,3,5,8,13,21,34,55,... 
-//		Faça um programa capaz de gerar a série até o n-ésimo termo.
+//		16. A série de Fibonacci é formada pela seqüência
+//		0,1,1,2,3,5,8,13,21,34,55,... Faça um programa que gere a série até
+//		que o valor seja maior que 500.
 		
 		Scanner scan = new Scanner (System.in);
 		
@@ -41,7 +42,6 @@ public class Exercicio15 {
 		} else {
 			System.out.println("Informe um número maior ou igual a zero.");
 		}
-		
 	}
 
 }
