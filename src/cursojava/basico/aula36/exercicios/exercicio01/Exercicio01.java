dab938e267900897e447dfa9fa1f4bfdf06f4008
home <home@home-desktop>
2018-08-16 00:12:10 -0300

    Início dos exercícios da aula 36.

--- src/cursojava/basico/aula36/exercicios/exercicio01/Exercicio01.java
+++ src/cursojava/basico/aula36/exercicios/exercicio01/Exercicio01.java
@@ -1,5 +1,7 @@
 package cursojava.basico.aula36.exercicios.exercicio01;
 
+import java.util.Scanner;
+
 public class Exercicio01 {
 
 	public static void main(String[] args) {
@@ -10,6 +12,11 @@ public class Exercicio01 {
 //		nome	da	Agenda	e	em	seguida	3	contatos.	Crie	métodos	que	retornem	
 //		uma	String	com	a	informação	de	cada	contato	e	também	de	todos	os	
 //		contatos	da	agenda.
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Informe o nome da agenda: ");
+		Agenda.setNome(scan.next());
 		
 		
 	}
