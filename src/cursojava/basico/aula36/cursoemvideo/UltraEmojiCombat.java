79e443f7de11eea76ac1a99776f5a21c88d17fd1
home <home@home-desktop>
2018-08-21 23:25:16 -0300

    Curso em vídeo: Exercício

--- src/cursojava/basico/aula36/cursoemvideo/UltraEmojiCombat.java
+++ src/cursojava/basico/aula36/cursoemvideo/UltraEmojiCombat.java
@@ -12,7 +12,12 @@ public class UltraEmojiCombat {
 		l[3] = new Lutador("Dead Code", "Austrália", 28, 1.93f, 81.6f, 13, 0, 2);
 		l[4] = new Lutador("UFOCobol", "Brasil", 37, 1.7f, 119.3f, 5, 4, 3);
 		l[5] = new Lutador("Nerdaart", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
+	
 		
-		l[3].status();		
+		Luta UEC01 = new Luta();
+		UEC01.marcarLuta(l[3], l[4]);
+		UEC01.lutar();
+		l[0].status();
+		l[1].status();
 	}
 }
