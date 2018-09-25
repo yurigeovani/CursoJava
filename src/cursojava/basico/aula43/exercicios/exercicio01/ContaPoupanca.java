2ace1721d8eeac72521dbee38e796fd9365d9e46
home <home@home-desktop>
2018-09-25 00:50:51 -0300

    Início exercícios aula 43.

--- src/cursojava/basico/aula43/exercicios/exercicio01/ContaPoupanca.java
+++ src/cursojava/basico/aula43/exercicios/exercicio01/ContaPoupanca.java
@@ -4,7 +4,24 @@ public class ContaPoupanca extends ContaBancaria {
 
 	double diaRendimento;
 	
-	public void calcularNovoSaldo() {
-		
+	public double getDiaRendimento() {
+		return diaRendimento;
+	}
+	public void setDiaRendimento(double diaRendimento) {
+		this.diaRendimento = diaRendimento;
+	}
+	
+	public ContaPoupanca() {
+		super();
+	}
+	public ContaPoupanca(String nomeCliente, int numConta, double saldo) {
+		super(nomeCliente, numConta, saldo);
+	}
+	
+	public void consultarSaldo2() {
+		System.out.println("super.getSaldo = " + super.getSaldo());
+		super.setSaldo(super.getSaldo()+(getDiaRendimento()*super.getSaldo()));
+		System.out.println("calc " + super.getSaldo());
+//		return super.getSaldo();
 	}
 }
